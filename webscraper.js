66e4f4dbb58759f56d34053fe11b099920f5ee5e
kylefenlon <kyle@kfenlon.com>
2023-02-18 18:45:42 +0200

    finished the first task

--- webscraper.js
+++ webscraper.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
-const require = require('fs')
+const fs = require('fs')
 
 axios.get('https://cdn.adimo.co/clients/Adimo/test/index.html')
     .then(function (response) {
@@ -8,28 +8,30 @@ axios.get('https://cdn.adimo.co/clients/Adimo/test/index.html')
         let products = []
         let totalPrice = 0
         let totalItems = 0
-        let discount = 0
+
 
         $('div.item').each(function (i, elem) {
             let product = {}
             product.title = $(elem).find('h1').text()
             product.imageUrl = $(elem).find('img').attr('src')
             product.price = parseFloat($(elem).find('span.price').text().replace(/[^0-9.-]+/g, ""))
-            product.oldPrice = parseFloat($(elem).find('span.oldPrice').text().replace(/[^0-9.-]+/g, ""))
+            product.discount = parseFloat($(elem).find('span.oldPrice').text().replace(/[^0-9.-]+/g, "")) - product.price;
+
+            if (product.discount < 0 || !product.discount) {
+                product.discount = 0;
+            }
 
             products.push(product)
             totalItems++
             totalPrice += product.price
-            discount = oldPrice - price;
         })
 
 
         let averagePrice = totalPrice / totalItems
         let output = {
-            products: products,
-            totalItems: totalItems,
-            averagePrice: averagePrice,
-            discount: discount
+            products,
+            totalItems,
+            averagePrice
         }
 
         fs.writeFile('output.json', JSON.stringify(output, null, 2), function (err) {
